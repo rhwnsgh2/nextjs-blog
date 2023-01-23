17bad0fc0c4c260733e9c25903d7f1b6da96bc35
junho <junho.ko@business-canvas.com>
2023-01-23 15:12:22 +0900

    test redirect type

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
         has: [
           {
             type: "host",
-            value: "*naver.com",
+            value: "(*naver.com*)",
           },
         ],
       },
