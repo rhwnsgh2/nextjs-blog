fc3b4656ec53b34394e7ad2e85757108a253d169
junho <junho.ko@business-canvas.com>
2023-01-23 14:33:25 +0900

    add redirect

---
+++ pages/posts/first-post.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+export default function FirstPost() {
+    return (
+        <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    );
+  }
